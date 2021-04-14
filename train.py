c718350a67b4e253b419496098d686f82beefe8e
minjoong507 <mjjung@bi.snu.ac.kr>
2021-04-14 18:43:24 +0900

    save results

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import torch
 from model.data_loader import get_dataloader, prepare_batch_input
 from model.model import BertCaptioning
-from utils import load_pickle
-from model.optimization import BertAdam
+from utils import load_pickle, start_time, mkdirp, write_log
 from vocab.make_vocab import Make_vocab, Vocab
 from config import BasicOption
 import argparse
@@ -12,29 +11,22 @@ import torch.optim as optim
 import pickle
 import os
 from tqdm import tqdm
-
 logger = logging.getLogger(__name__)
 
 
-# device = 'cuda:4' if torch.cuda.is_available() else 'cpu'
-# torch.cuda.set_device(device)
-# cuda = torch.device(device)
-# print(cuda)
-
-
-class Image_Captioning:
+class Bert_Captioning:
     def __init__(self):
-        self.args = BasicOption().parse()
-        self.vocab = load_pickle(self.args.vocab_path)
-        self.args.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-        torch.cuda.set_device(self.args.device)
-        print(self.args.device)
-        self.args.vocab_size = len(self.vocab)
-        self.DataLoader = get_dataloader(self.args)
-        self.Model = BertCaptioning(self.args, len(self.vocab))
+        self.config = BasicOption().parse()
+        self.vocab = load_pickle(self.config.vocab_path)
+        self.config.device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+        torch.cuda.set_device(self.config.device)
+        self.config.vocab_size = len(self.vocab)
+        self.DataLoader = get_dataloader(self.config)
+        self.Model = BertCaptioning(self.config, len(self.vocab))
 
     def translate(self, output, batch):
         outputs, inputs, targets = output.cpu(), batch['captions_input_ids'].cpu(), batch['captions_label'].cpu()
+        translate = ""
 
         for predict, input, target in zip(outputs, inputs, targets):
             _, predict = predict.max(dim=1)
@@ -43,47 +35,36 @@ class Image_Captioning:
             input = [self.vocab.idx2word[idx] for idx in input.tolist()]
             target = [self.vocab.idx2word[idx] for idx in target.tolist() if idx != -1]
 
-            print("predict : {} \n input : {}\n answer : {}\n".format(' '.join(predict),
-                                                                      ' '.join(input),
-                                                                      ' '.join(target)))
+            translate += ("predict : {} \n input : {}\n answer : {}\n".format(' '.join(predict),
+                                                                              ' '.join(input),
+                                                                              ' '.join(target)))
+        return translate
 
     def train(self):
-        self.Model.to(self.args.device)
-        # parameters = self.Model.parameters()
-        # num_train_optimization_steps = len(self.DataLoader) * self.args.epochs
-        # optimizer = BertAdam(parameters,
-        #                      lr=self.args.lr,
-        #                      t_total=num_train_optimization_steps,
-        #                      warmup=self.args.lr_warmup_proportion,
-        #                      schedule="warmup_linear")
+        mkdirp(self.config.result_path)
+        result_path = self.config.result_path + '/' + start_time()
+        mkdirp(result_path)
+        filename = result_path + '/' + 'train-log.txt'
 
-        optimizer = optim.Adam(self.Model.parameters(), lr=self.args.lr)
+        self.Model.to(self.config.device)
+        optimizer = optim.Adam(self.Model.parameters(), lr=self.config.lr)
 
         print("Now training")
         self.Model.train()
-        for epoch in range(self.args.epochs):
-            self.train_epoch(epoch, self.DataLoader, optimizer, self.Model)
-            # epoch_loss = 0.0
-            # last_batch = None
-            # last_output = None
-            #
-            # for batch_idx, batch in tqdm(enumerate(self.DataLoader)):
-            #     optimizer.zero_grad()
-            #
-            #     batch = prepare_batch_input(batch, self.args.device)
-            #     output, loss = self.Model(batch)
-            #
-            #     loss.backward()
-            #     optimizer.step()
-            #     epoch_loss += loss.item()
-            #
-            #     last_batch = batch
-            #     last_output = output
-            #
-            # print("Epoch : [{}/{}]\t Loss : {:.4f}".format(epoch, self.args.epochs, epoch_loss / len(self.DataLoader)))
-            # self.translate(last_output, last_batch)
+        for epoch in range(self.config.epochs):
+            self.train_epoch(epoch, self.DataLoader, optimizer, self.Model, filename)
 
-    def train_epoch(self, epoch, dataloader, optimizer, model):
+        checkpoint = {
+            "model": self.Model.state_dict(),
+            "model_cfg": self.Model.config,
+            "config": self.config,
+            "epoch": self.config.epochs
+        }
+
+        model_name = result_path + '/' + 'model.ckpt'
+        torch.save(checkpoint, model_name)
+
+    def train_epoch(self, epoch, dataloader, optimizer, model, filename):
         epoch_loss = 0.0
         last_batch = None
         last_output = None
@@ -91,7 +72,7 @@ class Image_Captioning:
         for batch_idx, batch in tqdm(enumerate(dataloader), desc=" Training =>", total=len(dataloader)):
             optimizer.zero_grad()
 
-            batch = prepare_batch_input(batch, self.args.device)
+            batch = prepare_batch_input(batch, self.config.device)
             output, loss = model(batch)
 
             loss.backward()
@@ -101,10 +82,15 @@ class Image_Captioning:
             last_batch = batch
             last_output = output
 
-        print("Epoch : [{}/{}]\t Loss : {:.4f}".format(epoch, self.args.epochs, epoch_loss / len(self.DataLoader)))
-        self.translate(last_output, last_batch)
+        epoch_result = "Epoch : [{}/{}]\t Loss : {:.4f}".format(epoch, self.config.epochs, epoch_loss / len(self.DataLoader))
+        translate = self.translate(last_output, last_batch)
+
+        result = epoch_result + '\n' + translate + '\n'
+        print(result)
+
+        write_log(filename, result)
 
 
 if __name__ == '__main__':
-    b = Image_Captioning()
+    b = Bert_Captioning()
     b.train()
