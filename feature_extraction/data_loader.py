27f8483b2e0aa1458c11a6e8712f8215cb0ded93
minjoong507 <mjjung@bi.snu.ac.kr>
2021-06-05 20:36:13 +0900

    add eval dataloader and eval images feature extraction module

--- feature_extraction/data_loader.py
+++ feature_extraction/data_loader.py
@@ -1,6 +1,5 @@
 import torch
 import nltk
-import pickle
 import numpy as np
 import os
 from tqdm import tqdm
@@ -17,16 +16,13 @@ logger = get_logger()
 class TrainDataLoader(data.Dataset):
     def __init__(self, config):
         self.config = config
-        # with open(self.config.vocab_path, 'rb') as f:
-        #     vocab = pickle.load(f)
-        # self.vocab = vocab
         self.coco = COCO(self.config.annotations_path)
         self.coco_ids = list(self.coco.anns.keys())
 
         if not os.path.isfile(self.config.coco_idx_path):
+            logger.info("Now get coco image id for train")
             self.preprocess_idx()
-        self.ids = list(np.load(self.config.coco_idx_path))
-
+        self.image_info = list(np.load(self.config.coco_idx_path))
         self.transform = transforms.Compose([
             transforms.RandomCrop(self.config.crop_size),
             transforms.RandomHorizontalFlip(),
@@ -36,7 +32,7 @@ class TrainDataLoader(data.Dataset):
             ])
 
     def __getitem__(self, idx):
-        data = self.coco.anns[self.ids[idx]]
+        data = self.coco.anns[self.image_info[idx]]
         img_id = str(data['image_id'])
 
         # load image data
@@ -46,9 +42,13 @@ class TrainDataLoader(data.Dataset):
         image = self.transform(image)
 
         return img_id, image
+        # image_id, image = self.image_info[idx]['image_id'], self.image_info[idx]['image_feat']
+        # image = self.transform(image)
+        #
+        # return image_id, image
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.image_info)
 
     def preprocess_idx(self):
         T = transforms.ToTensor()
@@ -68,7 +68,7 @@ class TrainDataLoader(data.Dataset):
             else:
                 preprocess.append(self.coco_ids[i])
 
-        logger.info('Saved coco idx file!')
+        logger.info('Saved train coco idx file!')
 
         np.save(self.config.coco_idx_path, np.array(preprocess))
 
@@ -81,6 +81,7 @@ class EvalDataLoader(data.Dataset):
         self.test_image_dict = load_json(self.config.eval_annotations_path)
 
         if not os.path.isfile(self.config.eval_coco_idx_path):
+            logger.info("Now get coco image id for eval")
             self.preprocess_idx()
         self.image_info = list(np.load(self.config.eval_coco_idx_path))
 
@@ -88,12 +89,13 @@ class EvalDataLoader(data.Dataset):
             transforms.RandomCrop(self.config.crop_size),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406),
-                                 (0.229, 0.224, 0.225))
             ])
 
     def __getitem__(self, idx):
-        image_id, image = self.image_info[idx]['image_id'], self.image_info[idx]['image_feat']
+        image_id = str(self.image_info[idx])
+        image_id = '0' * (12 - len(image_id)) + image_id
+        img_path = os.path.join(self.config.eval_img_path, image_id + '.jpg')
+        image = Image.open(img_path).convert('RGB')
         image = self.transform(image)
 
         return image_id, image
@@ -116,11 +118,11 @@ class EvalDataLoader(data.Dataset):
             if image.shape[1] < 224 or image.shape[2] < 224:
                 continue
             else:
-                preprocess.append({'image_id': image_id, 'image_feat': image})
+                preprocess.append(image_id)
 
-        logger.info('Saved coco idx file!')
+        logger.info('Saved test coco idx file!')
 
-        np.save(self.config.eval_annotations_path, np.array(preprocess))
+        np.save(self.config.eval_coco_idx_path, np.array(preprocess))
 
         return preprocess
 
--- feature_extraction/extraction.py
+++ feature_extraction/extraction.py
@@ -17,9 +17,9 @@ class Feature_extraction:
         self.config = config
         self.TrainDataloader = get_dataloader(self.config)
         self.EvalDataloader = get_eval_dataloader(self.config)
-        self.visual_feature = {}
+        self.visual_feature_train = {}
+        self.visual_feature_eval = {}
         self.Resnet = self.get_model()
-
 
     def get_model(self):
         model = ResNet()
@@ -32,21 +32,22 @@ class Feature_extraction:
 
     def run_extract(self, is_train=True):
         if is_train:
-            self.extract(self.TrainDataloader)
+            self.extract(self.TrainDataloader, self.config.train_visual_feature_path)
         else:
-            self.extract(self.EvalDataloader)
+            self.extract(self.EvalDataloader, self.config.test_visual_feature_path)
 
-    def extract(self, dataloader):
+    def extract(self, dataloader, visual_feature_path):
+        visual_feature = {}
         for batch_idx, (batch) in tqdm(enumerate(dataloader), desc=' Visual feature extraction', total=len(dataloader)):
-            image_id, image = list(batch[0]), batch[1].cuda()
+            image_id, image = batch[0], batch[1].cuda()
             with torch.no_grad():
                 output = self.Resnet(image)
 
             for i in range(len(image_id)):
-                self.visual_feature[image_id[i]] = output[i].cpu()
+                visual_feature[image_id[i]] = output[i].cpu()
 
-        logger.info("Saving the output features at {}".format(self.config.visual_feature_path))
-        save_pickle(self.visual_feature, self.config.visual_feature_path)
+        logger.info("Saving the output features at {}".format(visual_feature_path))
+        save_pickle(visual_feature, visual_feature_path)
 
 
 if __name__ == '__main__':
@@ -59,16 +60,16 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--device', default=0, type=int)
     parser.add_argument('--num_workers', default=4, type=int)
-    parser.add_argument('--visual_feature_path', default='data/visual_feature.pickle', type=str)
+    parser.add_argument('--train_visual_feature_path', default='data/visual_feature_train.pickle', type=str)
     parser.add_argument('--test_visual_feature_path', default='data/visual_feature_test.pickle', type=str)
-    parser.add_argument('--coco_idx_path', default='data/coco_idx.npy', type=str)
-    parser.add_argument('--eval_coco_idx_path', default='data/eval_coco_idx.npy', type=str)
+    parser.add_argument('--coco_idx_path', default='data/train_coco_idx.npy', type=str)
+    parser.add_argument('--eval_coco_idx_path', default='data/test_coco_idx.npy', type=str)
     parser.add_argument('--eval_annotations_path', default='data/annotations/image_info_test2017.json', type=str)
 
     args = parser.parse_args()
 
     feature = Feature_extraction(args)
-    # For train dataset image features
+    # # For train dataset image features
     feature.run_extract()
 
     # For eval dataset test features
